b45379ac8b65507b98e8e0bc2fcd63cdc2d79cef
Dorian-Gray73 <48696626+Dorian-Gray73@users.noreply.github.com>
2021-11-05 10:29:47 +0100

    Javadoc :x

--- fc/CarteAbonnement.java
+++ fc/CarteAbonnement.java
@@ -1,5 +1,13 @@
 package fc;
 
+/**
+ * Classe représentant une carte d'abonnement.<br>
+ * Une carte d'abonnement est identifiée par un solde, un tableau de chaine presentant 
+ * les restrictions présente sur la carte, la carte mère de la carte, 
+ * un boolean qui indique si la carte est bloquée et un nombre de location
+ * 
+ */
+
 public class CarteAbonnement {
     private double solde;
     private String[] restriction;
@@ -7,6 +15,11 @@ public class CarteAbonnement {
     private boolean bloque;
     private int nbLocation;
     
+    /***
+     * Constructeur de la carte d'abonnement<br>
+     * Ce constructeur sera appelé dans le cas où la carte est une carte fille 
+     * @param mere carte d'abonnement mere
+     */
     public CarteAbonnement(CarteAbonnement mere) {
         this.solde = 10.0;
         this.restriction = null;
@@ -14,16 +27,22 @@ public class CarteAbonnement {
         this.nbLocation = 0;
         this.carteMere = mere;
     }
-
+    /**
+     * Constructeur de la carte d'abonnement<br>
+     * Ce constructeur sera appelé s'il n'a pas de carte mère.<br>
+     * La carte nouvellement créée sera donc sa propre carte mère.
+     */
     public CarteAbonnement(){
         this(null);
     }
 
     /**
      * Methode qui permet de débiter une carte <br>
-     * 
-     * @param prix
-     * @return boolean
+     * Cette méthode tient compte de si le client a deja loué plus de 20 films. <br>
+     * S'il a loué plus de 20 films alors la location lui est offert
+     * @param prix désigle le tarif de la location
+     * @return boolean true si la location c est bien passé <br>
+     * et false si la carte c'est bloqué
      */
     public boolean debiterCarte(double prix) {
         int nbLocation = getnbLocation();
@@ -36,14 +55,16 @@ public class CarteAbonnement {
                 setBlocage(true);
             }
             setnbLocation(nbLocation + 1);
-            return ! getBlocage();
+            return !getBlocage();
         }
     }
 
     /**
-     * @return boolean
+     * fonction qui retourne si le solde sur une carte est positif
+     * @return boolean qui représente si le solde est suffisant 
      */
     private boolean SoldeInsuffisant() {
+        //TODO pk on met pas le prix d'une location ? 
         return solde < 0;
     }
 
@@ -53,82 +74,102 @@ public class CarteAbonnement {
      */
     public boolean crediterCarte(double somme) {
         this.solde += somme;
+        if (!SoldeInsuffisant()) {
+            setBlocage(false);
+        }
         return true;
     }
 
     /**
-     * @param restriction
+     * fonction qui permet de définir les restrictions sur les cartes enfants ou 
+     * sur soit meme s'il n'y a pas de carte enfant<br> 
+     * @param restriction tableau de String contenant les restrictions
      */
     public void setRestriction(String[] restriction) {
         this.restriction = restriction;
     }
 
     /**
-     * @return restriction
+     * Retourne les restrictions appliqué sur la carte 
+     * @return String[]
      */
     public String[] getRestriction() {
         return restriction;
     }
 
     /**
-     * @param boolean
+     * Fonction qui permet de bloquer / debloquer une carte. 
+     * Cela s'effectue en changeant la valeur du boolean bloque
+     * @param boolean nouvelle etat de la carte
      */
     public void setBlocage(Boolean bloque) {
         this.bloque = bloque;
     }
 
     /**
-     * @return boolean
+     * Fonction retournant l'etat de la carte
+     * @return boolean true si la carte est bloque
      */
     public boolean getBlocage() {
         return bloque;
     }
 
     /**
-     * @return int
+     * Fonction donnant le nombre de location en cours
+     * @return int representant le nombre de location en cours
      */
     public int getnbLocation() {
         return nbLocation;
     }
 
     /**
-     * @param nbLocation
+     * Fonction qui définit le nombre de location
+     * @param nbLocation nouveau nombre de location
+     * @return void
      */
     public void setnbLocation(int nbLocation) {
         this.nbLocation = nbLocation;
     }
 
     /**
-     * @return boolean
+     * Fonction qui indique si une carte est une carte mère ou non
+     * @return boolean qui est true si la carte est une carte mère
      */
     public Boolean estCarteMere() {
         return (this.carteMere == this);
     }
 
+    /** 
+     * Fonction qui retourne le solde de la carte
+     * @return double solde de la carte
+     */
+    private double getSolde() {
+        return solde;
+    }
+    
+    /** 
+     * Fonction qui retourne la carte mère de la carte interrogée
+     * @return CarteAbonnement la carte mère de la carte interrogée
+     */
+    private CarteAbonnement getCarteMere() {
+        return carteMere;
+    }
+    
+    /** 
+     * Fonction qui redefini la fonction tostring.<br>
+     * Permet d'avoir un affichage plus comprehensible 
+     * @see Object.toString
+     * @return String
+     */
     @Override
     public String toString() {
         return "{" +
         " solde = '" + getSolde() + "'" +
         ", restriction = '" + getRestriction() + "'" +
         ", cartemère = '" + getCarteMere() + "'" +
-        ", bloque = '" + isBloque() + "'" +
-        ", nbLocation = '" + getNbLocation() + "'" +
+        ", bloque = '" + getBlocage() + "'" +
+        ", nbLocation = '" + getnbLocation() + "'" +
         "}";
     }
-
-    private CarteAbonnement getCarteMere() {
-        return carteMere;
-    }
-
-    private int getNbLocation() {
-        return nbLocation;
-    }
-
-    private boolean isBloque() {
-        return bloque;
-    }
-
-    private double getSolde() {
-        return solde;
-    }
+    
 }
