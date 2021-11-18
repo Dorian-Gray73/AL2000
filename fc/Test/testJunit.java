705330d4b29820cb685d5d1cb8634c336853ec2f
Dorian-Gray73 <48696626+Dorian-Gray73@users.noreply.github.com>
2021-11-18 22:28:50 +0100

    test Junit

--- fc/Test/testJunit.java
+++ fc/Test/testJunit.java
@@ -1,12 +1,12 @@
 package fc.Test;
 
+//import org.junit.Test;
+//import static org.junit.Assert.assertEquals;
 public class testJunit {
-    /* 
-    @test
-    void testDebiter(){
-        CarteAbonnement ca = new CarteAbonnement();
-    }
+    // @test
+
+    // void testDebiter(){
+    //     CarteAbonnement ca = new CarteAbonnement();
+    // }
     
-    test debiter cartes
-    */
 }
