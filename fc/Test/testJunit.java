f90cbdb6a5b564e72c761fb033b79550c6ea11c5
Dorian-Gray73 <48696626+Dorian-Gray73@users.noreply.github.com>
2021-11-18 11:15:44 +0100

    test

---
+++ fc/Test/testJunit.java
@@ -0,0 +1,14 @@
+package fc.Test;
+
+import fc.CarteAbonnement;
+
+public class testJunit {
+    
+    @test
+    void testDebiter(){
+        CarteAbonnement ca = new CarteAbonnement();
+    }
+    /*
+    test debiter cartes
+    */
+}
