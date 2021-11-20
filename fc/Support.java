f429d274c08570691bf94c0ebb1608b14700aee3
Dorian-Gray73 <48696626+Dorian-Gray73@users.noreply.github.com>
2021-11-20 10:50:20 +0100

    doc
    Co-authored-by: Daemianas <Daemianas@users.noreply.github.com>

--- fc/Support.java
+++ fc/Support.java
@@ -2,25 +2,23 @@ package fc;
 /**
  * Classe représentant un Support <br>
  * Un support sera défini par un film.<br>
- * @see Film
+ * @see Film classe extremement lier a support. Nous vous conseillons donc de ne pas hesite a consulter la classe Film
  * Cette classe dispose des methodes afficherInformations et calculDuree 
  */
 public class Support {
     private Film film;
     /**
      * Constructeur de la classe Support
-     * @param film designe le film qui est soit un QRCode soit un dvd
+     * @param film designe le film qui est soit un QRCode soit un cd
      * 
      */
-
     public Support(Film film) {
         this.film = film;
     }
 
     /**
      * Fonction qui permet d'afficher les informations d'un film
-     * @see Film
-     * @return void
+     * @see Film afin d obtenir des informations sur la methode afficherInformations()
      */
     public void afficherInformations() {
         film.afficherInformations();
@@ -42,8 +40,8 @@ public class Support {
     /** 
      * Fonction qui redefini la fonction tostring.<br>
      * Permet d'avoir un affichage plus comprehensible 
-     * @see Object
-     * @return String
+     * @see Object pour voir la definition de toString()
+     * @return String une représentation du support
      */
     @Override
     public String toString() {
@@ -55,7 +53,7 @@ public class Support {
     
     /** 
      * Fonction qui retourne le film 
-     * @return Film 
+     * @return Film du support
      */
     public Film getFilm() {
         return film;
@@ -64,7 +62,7 @@ public class Support {
     
     /** 
      * Fonction qui sera redefini dans la classe CD
-     * @see CD
+     * @see CD ou la methode sera defini
      * @param dommage parametre qui représente l'état du support
      */
     public void setEndommage(boolean dommage){
