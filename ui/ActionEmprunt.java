4846668fc8681e1a70b39924a8a5c8798d5656e7
Dorian-Gray73 <48696626+Dorian-Gray73@users.noreply.github.com>
2021-11-26 00:25:24 +0100

    ajout excpetion carte bloqué

--- ui/ActionEmprunt.java
+++ ui/ActionEmprunt.java
@@ -3,6 +3,8 @@ package ui;
 import java.awt.event.ActionEvent;
 import javax.swing.JOptionPane;
 import javax.swing.AbstractAction;
+
+import fc.CD;
 import fc.ErreurEmpruntException;
 import fc.FacadeNf;
 import fc.Support;
@@ -21,11 +23,17 @@ public class ActionEmprunt extends AbstractAction {
 	public void actionPerformed(ActionEvent e) {
 		try {
 			int code = f.emprunt(s);
-			f.setCDEnCours(s);
+			if (s instanceof CD) {
+				f.setCDEnCours(s);
+			} 
+			
+
 			JOptionPane.showMessageDialog(null, "Le numéro de votre emprunt est : " + code, "Retenu",
 					JOptionPane.INFORMATION_MESSAGE);
 		} catch (ErreurEmpruntException e1) {
 			e1.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Votre solde n'est pas suffisant. ", "Carte bloquée",
+					JOptionPane.ERROR_MESSAGE);
 		}
 	}
 }
